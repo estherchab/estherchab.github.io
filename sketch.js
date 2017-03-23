f838939e3aaf11d9efc7de927ccd742a523727e6
Esther Chabbott <Esther@Esthers-MacBook-Air-2.local>
2017-03-23 11:42:22 -0400

    initial commit

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(720, 400);
+
+  // Set colors
+  fill(204, 101, 192, 127);
+  stroke(127, 63, 120);
+
+
+  translate(580, 200);
+  noStroke();
+  for (var i = 0; i < 10; i ++) {
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+}
+translate(580, 200);
+noStroke();
+for (var i = 0; i < 10; i ++) {
+  ellipse(0, 30, 20, 80);
+  rotate(PI/5);
+}
+}
